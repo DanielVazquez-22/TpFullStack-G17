cfa1c12712813461e33ea1fa6c4524def5cdf79c
LeoEze90 <emontenegro136@gmail.comgit config --global user email.emontenegro136@gmail.comgit config --glbal user.email emontenegro136@gmail.com config --global user.email emontenegro136@gmail.comgit config --global user.email emontenegro136@gmail.com>
2024-05-20 20:38:28 -0300

    intentoFuncion

--- static/js/masInfo.js
+++ static/js/masInfo.js
@@ -2,28 +2,11 @@ ver(i)
 parseInt(i)
 function masInfo () {
     if (i===1) {
-            (<div class="resume">
-                <h3 class="title">One Piece</h3>
-                <p class="description">Una historia épica de piratas, donde narra la historia de "Monkey D. Luffy" quien cuado tenia 7 años, comió accidentalmente una "Akuma no mi"(Futa del diablo) la cual le dio poderes de goma. Por otra parte "Gol D. Roger" conocido como "El rey de los Piratas" quien fuera ejecutado por la Marine, habló antes de morir, acerca de su famoso tesoro "One Piece" escondido en la "Gran line". Esta noticia desato la gran era de la piratas lanzando a incontables piratas a ese lugar, en busca de "One Piece" el tesoro perdido. Diez años después, Luffy inspirado en "Gol D. Roger" y un pirata de nombre Akagami no Shanks (Shanks el pelirrojo) se convierte en pirata deseando ser el próximo "Rey de los Piratas" y zarpar para conocer amigos y tener aventuras con ellos, teniendo como meta encontrar el "One Piece".</p>
-                </div>
-            <div class="trailer">
-                <iframe max-width=100% aspect-ratio=16/9 width=560 height=315 src='https://www.youtube.com/embed/CWrJQzm0diI?si=fTnwXM4pr12uSoue' title=YouTube video player frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            </div>
-            <div classs="info">
-                <table>
-                    
-                    <tr>
-                        <td>Título Original:</td>   
-                        <td id="tiInf">One Piece (ワンピース)</td>  
-                    </tr>
-                    
-                    <tr>
-                        <td>Estado de la serie:</td>
-                        <td id="status">En emision...</td>    
-                    </tr>
+        document.write (element.createElement (innerHTML(
+           
+    )))
             
-                </table>
-            </div>)
+            
 
     }
 
